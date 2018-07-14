34a19075265ad3765e1fc98f0fa837f8e34cd3e4
张玮 <zhangwei@zhangweideMacBook-Air.local>
2018-07-14 09:35:08 +0800

    上传工程

---
+++ ONE/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ONE
+//
+//  Created by 张玮 on 2018/7/14.
+//  Copyright © 2018年 张玮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
